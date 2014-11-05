883f143536f84f0acd77bdf0bb2a71956badf948
Paul Bird <paulbird1993@gmail.com>
2014-11-05 15:35:36 +0000

    added front-page.php and .twig + any needed styles

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+// Add theme support for thumbnails, such as posts and pages etc
+add_theme_support('post-thumbnails');
+
+//registers a new size you can use, isn't shown in the admin unless added
+add_image_size('HD',1920,1080,false);
+
+ ?>
